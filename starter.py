15072c1a17cfe2086031c2a262ca0b99c52f6213
Ryan Davis <ryancraigdavis@github.com>
2024-02-14 10:25:08 -0800

    1 Attempting to fix service context

--- starter.py
+++ starter.py
@@ -1,15 +1,23 @@
 import sys
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+import os
+from llama_index.core import ServiceContext, StorageContext, VectorStoreIndex, SimpleDirectoryReader, Settings, load_index_from_storage
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.llms.ollama import Ollama
 
 def query_wowhead(query: str):
-    documents = SimpleDirectoryReader("data").load_data()
-    Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
-    Settings.llm = Ollama(model="mistral", request_timeout=30.0)
-    index = VectorStoreIndex.from_documents(
-        documents,
-    )
+    PERSIST_DIR = "./storage"
+    if not os.path.exists(PERSIST_DIR):
+        documents = SimpleDirectoryReader("data").load_data()
+        Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
+        Settings.llm = Ollama(model="mistral", request_timeout=30.0)
+        index = VectorStoreIndex.from_documents(
+            documents,
+        )
+        index.storage_context.persist(persist_dir=PERSIST_DIR)
+    else:
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        index = load_index_from_storage(storage_context)
+        ServiceContext.from_defaults(embed_model="local:BAAI/bge-small-en-v1.5", llm=Ollama(model="mistral", request_timeout=30.0))
     query_engine = index.as_query_engine()
     response = query_engine.query(query)
     return response
